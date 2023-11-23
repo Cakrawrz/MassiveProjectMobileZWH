a1a2c75922fd640933a31050fd77a9d72551fd45
chuda203 <hudkhoir203@gmail.com>
2023-11-23 14:16:12 +0700

    Tambah layout pengguna, beranda "penjemputan sampah" 70 %

---
+++ app/src/main/java/com/infinite/massiveprojectmobilezwh/BerandaA1Activity.kt
@@ -0,0 +1,11 @@
+package com.infinite.massiveprojectmobilezwh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BerandaA1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beranda_a1)
+    }
+}
---
+++ app/src/main/java/com/infinite/massiveprojectmobilezwh/BerandaA4Activity.kt
@@ -0,0 +1,11 @@
+package com.infinite.massiveprojectmobilezwh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BerandaA4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beranda_a4)
+    }
+}
